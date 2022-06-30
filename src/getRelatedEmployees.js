5861957d99b9e13e3ebc758a5a4fcc99ee1984e9
Carina Cunha <carina.cunha13@gmail.com>
2022-06-30 20:09:11 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const elementResults = employees.map((employee) => employee.managers.includes(id));
+  return elementResults.some((element) => element === true);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const isManagerResult = isManager(managerId);
+  if (isManagerResult === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const elementResults = employees.filter((employee) => employee.managers.includes(managerId));
+  return elementResults.map((element) => `${element.firstName} ${element.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
