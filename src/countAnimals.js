4127f7887c612dee0f64c76c71e823cc6bd204ce
Carina Cunha <carina.cunha13@gmail.com>
2022-06-30 00:42:54 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(inputObj) {
+  if (!inputObj) {
+    return species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  } if (!inputObj.sex) {
+    const { specie: inputValue } = inputObj;
+    const targetSpecies = species.find((element) => element.name === inputValue);
+    return targetSpecies.residents.length;
+  }
+  const { specie: inputValue, sex: sexValue } = inputObj;
+  const targetResidents = species.find((element) => element.name === inputValue).residents;
+  return targetResidents.filter((element) => element.sex === sexValue).length;
 }
 
+console.log(countAnimals());
+console.log(countAnimals({ specie: 'penguins' }));
+console.log(countAnimals({ specie: 'penguins', sex: 'female' }));
 module.exports = countAnimals;
