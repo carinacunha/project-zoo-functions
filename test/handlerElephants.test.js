9ff6e2d2a0de487fc3334219c95fdbe839957d15
Carina Cunha <carina.cunha13@gmail.com>
2022-06-30 21:29:00 -0300

    Requisitos 6 e 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,23 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  test('Testa comportamento com parâmetro indefinido', () => {
+    expect(handlerElephants()).toBe(undefined);
+  });
+
+  test('Testa se o parametro é string', () => {
+    expect(handlerElephants(0)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+
+  test('Testa o parametro count', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+
+  test('Testa o parametro names', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+
+  test('Testa o parametro averageAge', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5, 2);
+  });
+});
